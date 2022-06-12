121e92ffde4f763f31898073a01cdb557e81a8cf
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-06-12 20:35:52 -0300

    feat: create client

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+class CreateClientUseCase {
+  async execute({ username, password }: ICreateClient) {
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive"
+        }
+      }
+    });
+
+    if (clientExists) {
+      throw new Error("Username already exists!");
+    }
+
+    const passwordHash = await hash(password, 10);
+
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: passwordHash
+      }
+    });
+
+    return client;
+  }
+}
+
+export { CreateClientUseCase }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Hello World!"
-  })
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server is running!");
